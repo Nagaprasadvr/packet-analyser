b22d9093e058029cb1defac8018c2c79cbf7a7ae
Nagaprasadvr <nagaprasadvr246@gmail.com>
2022-05-17 22:29:26 +0530

    changes

--- PcapAnalyser/PcapAnalyser/settings.py
+++ PcapAnalyser/PcapAnalyser/settings.py
@@ -9,11 +9,12 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
 
 
 # Quick-start development settings - unsuitable for production
@@ -39,6 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'PcapAnalyserApp.apps.PcapAnalyserAppConfig'
 ]
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
